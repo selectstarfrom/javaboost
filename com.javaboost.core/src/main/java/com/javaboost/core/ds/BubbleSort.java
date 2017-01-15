f2d6e5d2e0e7a3573fb4b03b386378ec9eb568dc
s726346 <s726346@PC3041571159.corp.emirates.com>
2017-01-15 09:07:36 +0400

    bubble sort

--- com.javaboost.core/src/main/java/com/javaboost/core/ds/BubbleSort.java
+++ com.javaboost.core/src/main/java/com/javaboost/core/ds/BubbleSort.java
@@ -1,11 +1,13 @@
 package com.javaboost.core.ds;
 
+import java.util.Arrays;
+
 public class BubbleSort {
 	public static void main(String[] args) {
 
 		int[] lArray = { 1, 4, 15, 3, 6, 7, 9, 2, 3, 11, 99, 0, 3 };
 
-		System.out.println("INPUT: " + lArray);
+		System.out.println("INPUT (" +lArray.length+"):"+ Arrays.toString(lArray));
 		for (int lI = 0; lI < lArray.length; lI++) {
 
 			for (int lI2 = 0; lI2 < (lArray.length-1); lI2++) {
@@ -15,13 +17,13 @@ public class BubbleSort {
 
 				if (lFirst > lSecond) {
 					int lTemp = lFirst;
-					lFirst = lSecond;
-					lSecond = lTemp;
+					lArray[lI2] = lSecond;
+					lArray[lI2+1] = lTemp;
 				}
 			}
 
 		}
 
-		System.out.println("OUTPUT: " + lArray);
+		System.out.println("OUTPUT: " + Arrays.toString(lArray));
 	}
 }
